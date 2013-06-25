67c1aade797943e55cd64e464a2ba2c22d0bafe5
Fabio Rizzo <fabiorizzo@liberiun.com>
2013-06-25 13:56:28 -0300

    Correcao do Tile Como Fazer

--- vindula/tile/browser/howdo.py
+++ vindula/tile/browser/howdo.py
@@ -1,21 +1,21 @@
 # coding: utf-8
 from five import grok
 from vindula.tile.browser.baseview import BaseView
+import string
 
 grok.templatedir('templates')
 
 class HowDoView(BaseView):
     grok.name('howdo-view')
+    first_active = True
 
     def getItens(self):
         context = self.context
         numbers = context.getNumItems()
         types = context.getObjectType()
         subjects = context.getSubjects()
-        
+	
 
-#        path = context.getPath()
-#        if not path:
         path = context.portal_url.getPortalObject()
 
         itens = self.portal_catalog(portal_type = types,
@@ -27,3 +27,67 @@ class HowDoView(BaseView):
                                     )
 
         return itens[:numbers]
+
+    def alphabet(self):
+        #ABCDEFGHIJKLMNOPQRSTUVWYXZ
+        L = []
+        for i in string.ascii_uppercase:
+            L.append(i)
+
+        return L
+
+    def constructor(self):
+        letras = {}
+        for i in self.alphabet():
+            letras[i] = []
+
+        for item in self.list_more():
+            obj = item.getObject()
+            title = self.getTitle(obj)
+            try:
+                letras[title[0]].append(item)
+            except KeyError:
+                pass
+
+
+        return letras
+
+    def list_more(self):
+        context = self.context
+        path = context.portal_url.getPortalObject()
+
+        portal_type = context.getObjectType()
+
+        subjects = context.getSubjects()
+
+
+        itens = self.portal_catalog(portal_type = portal_type,
+                                    # review_state = states,
+                                    path={'query':'/'.join(path.getPhysicalPath()),'depth':99},
+                                    sort_on='effective',
+                                    Subject=subjects,
+                                    sort_order='descending',
+                                    )
+
+        return itens
+
+
+    def getTitle(self,obj):
+        str = ''
+        if hasattr(obj, 'getSiglaunidade'):
+            str = obj.getSiglaunidade().upper()
+
+        if not str:
+            str = obj.Title().upper()
+
+        # print str
+        return str
+
+
+    def is_first_active(self,has_content):
+        if not has_content and self.first_active:
+            self.first_active = False
+            return 'active'
+        else:
+            return ''
+
