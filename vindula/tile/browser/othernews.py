10493a5c55b2fbf7ebc4297a94d108c9934a4e2c
Matheus Franca <matheusfranca@liberiun.com>
2013-10-03 16:22:09 -0300

    Other news

---
+++ vindula/tile/browser/othernews.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from five import grok
+from Products.CMFCore.utils import getToolByName
+
+from vindula.tile.browser.baseview import BaseView
+
+
+grok.templatedir('templates')
+
+class OtherNewsView(BaseView):
+    grok.name('outras-noticias')
+    
+    
+    
+    def getQuery(self):
+        context = self.context
+        query = {}
+        portal_catalog = getToolByName(context, 'portal_catalog')
+        path = context.portal_url.getPortalObject().getPhysicalPath()
+
+
+        form = self.request.form
+        
+        
+        if 'path' in form.keys():
+            query.update({'path': form.get('path', '')})
+            
+        else:
+            query.update({'path': {'query':'/'.join(path) } })
+        
+        
+        if 'portal_type' in form.keys():
+            query.update({'portal_type': form.get('portal_type', '')})
+            
+        else:
+            query.update({'portal_type': ('VindulaNews', 'News Item') })
+        
+
+
+        query.update({'review_state': ['published', 'internally_published', 'external']})
+        
+        
+        query.update({'sort_on':'created',
+                      'sort_order':'descending',
+                     })
+
+        result = portal_catalog(**query)
+        
+        return result
+        
+        
+        
+        
+
