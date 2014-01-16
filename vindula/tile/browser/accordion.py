bb3bc30ff36e5363a5d4b66baa7775b72b954293
Cesar Augusto <cesaraugusto@liberiun.com>
2014-01-16 15:09:47 -0200

    implementação do controle de permição nas abas do bloco de acordion

--- vindula/tile/browser/accordion.py
+++ vindula/tile/browser/accordion.py
@@ -7,3 +7,20 @@ grok.templatedir('templates')
 class AccordionView(BaseView):
     grok.name('accordion-view')
 
+
+    def get_itens_abas(self):
+        context = self.context	
+        current_user = self.p_membership.getAuthenticatedMember()
+        L = []
+
+        itens = self.portal_catalog(**{'sort_on': 'getObjPositionInParent',
+	                                   'portal_type':['TileAccordionItem',],  
+	                                   'path':{'query':'/'.join(context.getPhysicalPath()), 'depth': 1}
+                                	})
+
+        for t in itens:
+            t = t.getObject()
+            if not t.getExcludeFromNav() and self.has_public_or_permission(current_user, t):
+                L.append(t)
+
+        return L
