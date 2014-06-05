71f8d48ac268a3bcb6fd6f4b7a955e6a7b64a984
Denis Raison <denisraison@liberiun.com>
2014-06-05 10:29:50 -0300

    Melhorias no bloco de video

--- vindula/tile/browser/tilemultimedia.py
+++ vindula/tile/browser/tilemultimedia.py
@@ -6,3 +6,18 @@ grok.templatedir('templates')
 
 class TileMultimediaView(BaseView):
     grok.name('tilemultimedia-view')
+    
+    
+    def createScript(self, id):
+        
+        return '''
+            flowplayer("%s", "flowplayer-3.1.2.swf", {
+                clip: {
+                    autoPlay: false,
+                    autoBuffering: false,
+                    showMenu: false,
+                    autoRewind: true,
+                    loop: false
+                }
+            });
+        ''' % (id)
