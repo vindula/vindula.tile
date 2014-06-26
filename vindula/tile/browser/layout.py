07461446b3d859205a5b09909ef1e22813ccf588
Denis Raison <denisraison@liberiun.com>
2014-06-26 17:34:52 -0300

    Ajustes drag in drop

--- vindula/tile/browser/layout.py
+++ vindula/tile/browser/layout.py
@@ -21,9 +21,9 @@ class LayoutView(BaseView):
         path_js = []
 
         # #Adição do js de edição dos blocos via modal
-        scripts_js.append('tile-edit.js')
+        path_js.append('tile-edit.js')
         # #Adição do js de drag n drop dos blocos
-        scripts_js.append('tile-sortable.js')
+        path_js.append('tile-sortable.js')
 
 
         #Coleta dos Script js dos tiles
